69f0e60d7890cfd5bbd70024538c51de12c49627
ospdm <vfv89877@gmail.com>
2025-06-09 07:22:58 +0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -12,6 +12,7 @@ def is_palindrome(s: str) -> bool:
 if __name__ == "__main__":
     s = input("Введите строку: ")
     if is_palindrome(s):
+    # master: проверяем ввод пользователя
         print("Это палиндром!")
     else:
         print("Это не палиндром.")
